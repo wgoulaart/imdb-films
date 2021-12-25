2dafcfa9cc4951221cf98882ceaa468fcd0a93bf
William Goulart <williamgoulart1@hotmail.com>
2021-12-25 17:07:07 -0300

    feat(Jest): Configuração jest, babel e dependencias

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/components/*.ts(x)?',
+    '!src/components/stories.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/']
 }
