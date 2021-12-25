b6aab0ba6ae1f1d689f70b8b843db920b580fa7d
William Goulart <williamgoulart1@hotmail.com>
2021-12-25 17:09:43 -0300

    feat(App/Document): Configurando heading, google fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import '@/styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>GazimFilms</title>
+        <meta name="theme-color" content="#030518" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
