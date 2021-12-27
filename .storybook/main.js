8217c6f0ee471fe85bdfd3237991fea7bce4d187
William Goulart <williamgoulart1@hotmail.com>
2021-12-27 16:34:28 -0300

    refactor(Packages): Adicionando storybook, styled-components e alterando configurações(ts,eslint...)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,10 @@
 module.exports = {
-  stories: ['../src/components/**/stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react'
+  framework: '@storybook/react',
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    console.log(`process.cwd():: ${process.cwd()}/src`)
+    return config
+  }
 }
