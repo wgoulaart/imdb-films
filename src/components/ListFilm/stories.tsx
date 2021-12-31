a10d60502541c93a4a469859e0875ebf53b0f37c
William Goulart <williamgoulart1@hotmail.com>
2021-12-31 15:18:15 -0300

    feat(ListFilm): Criando componente de listagem de filmes

--- src/components/ListFilm/stories.tsx
+++ src/components/ListFilm/stories.tsx
@@ -2,8 +2,12 @@ import { Story, Meta } from '@storybook/react'
 import ListFilm from '.'
 
 export default {
-  title: 'ListFilm',
+  title: 'Components/ListFilm',
   component: ListFilm
 } as Meta
 
 export const Default: Story = (args) => <ListFilm {...args} />
+
+Default.args = {
+  title: 'Filmes populares'
+}
