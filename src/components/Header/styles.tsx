06cdcf6bad3e8e8dd33cba83df54496dc073aff3
William Goulart <williamgoulart1@hotmail.com>
2021-12-31 15:16:29 -0300

    feat(Header): Criando componente Header

---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.header`
+  display: flex;
+  align-items: center;
+  padding: var(--size-base);
+  background: var(--color-dark);
+  gap: var(--size-base);
+`
