0a4dbece145303c745c7d605f9a12e9b793bd8c2
William Goulart <williamgoulart1@hotmail.com>
2022-01-04 04:31:42 -0300

    wip: pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,23 +6,80 @@ import ListFilm from '../components/ListFilm'
 import Footer from '../components/Footer'
 import ListActor from 'components/ListActor'
 import Container from 'components/Container'
+import { useEffect, useState, useCallback } from 'react'
 
-import API from 'service/api'
+import api from 'service/api'
 
 const Home: NextPage = () => {
+  const [listPopular, setListPopular] = useState([])
+  const [filmsListPopular, setFilmsListPopular] = useState([])
+
+  const getTitles = useCallback(async () => {
+    let titleSerialized = []
+
+    console.log(`getTitles`)
+    await api.getTitlesMostPopular().then((response) => {
+      console.log(`getTitles then(response)`, response)
+
+      titleSerialized = response?.data.map((element: string) => {
+        return element.replace('/title/', '').replace('/', '')
+      })
+      setListPopular(titleSerialized)
+    })
+  }, [])
+
+  const getMovies = useCallback(async (title?: string) => {
+    getMovieData(title)
+    console.log(`getMovies`, title)
+    // await api.getMostPopularMovies(title).then((response) => {
+    //   setFilmsListPopular(response?.data)
+    //   // setFilmsListPopular((prev): any[] => [...prev, response.data])
+    // })
+  }, [])
+
+  function getMovieData(items): void {
+    console.log(`getMovieData::`, items)
+
+    api.getMostPopularMovies(items).then((response) => {
+      console.log(`getMovieData:: response`, response)
+      setFilmsListPopular(response?.data)
+    })
+    //   console.log(`getMovieData listPopular[index]::`, items[index])
+    // for (let index = 0; index < initStateRequest; index++) {
+    // await api
+    //   .getMostPopularMovies(title)
+    //   .then((response) => setFilmsListPopular(response?.data))
+    // }
+  }
+
+  useEffect(() => {
+    getTitles()
+  }, [getTitles])
+
+  useEffect(() => {
+    if (listPopular === undefined) return
+    getMovies(listPopular[0])
+    console.log(`filmsListPopular::`, filmsListPopular)
+  }, [getMovies, filmsListPopular, listPopular])
+
+  //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //   // @ts-ignore
+  //   setFilmsListPopular((prevState) => [...prevState, { state }])
+
   return (
     <>
       <Header />
       <Banner />
       <Container>
-        <ListFilm title="Filmes populares" items={API.getMostPopularMovies} />
+        {/* <ListFilm title="Filmes populares" items={API.getMostPopularMovies} /> */}
+        <ListFilm title="Filmes populares" items={[]} />
 
-        <ListFilm
-          title="Próximos lançamentos"
-          items={API.getComingSoonMovies}
-        />
+        {/* <ListFilm
+              title="Próximos lançamentos"
+              items={API.getComingSoonMovies}
+            />
 
-        <ListActor title="Atores populares" items={API.getActorsList} />
+            <ListActor title="Atores populares" items={API.getActorsList} /> */}
       </Container>
       <Footer />
     </>
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+
+import Container from 'components/Container'
+import Footer from 'components/Footer'
+import Header from 'components/Header'
+import ListResults from 'components/ListResults/intex'
 
 const Search: NextPage = () => {
-  return <h1>Search</h1>
+  const router = useRouter()
+  const [busca, setBusca] = useState([])
+  const search = router.query.search
+  useEffect(() => {
+    console.log(`searchPage:`, search)
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <Container>
+        {busca.map((buscar, index) => {
+          return (
+            <h1 key={index}>
+              {buscar} - parametro da busca: {search}
+            </h1>
+          )
+        })}
+      </Container>
+      <Footer />
+    </>
+  )
 }
 
 export default Search
